39bf29322d94aeee5279f43e92e5ed6ca93284fc
asatvilker <asatvilker@users.noreply.github.com>
2021-02-23 22:30:34 +0000

    integrated api to summary section in overview

--- src/components/DropDown/overview.js
+++ src/components/DropDown/overview.js
@@ -7,11 +7,23 @@ class Overview extends Component {
 state = {
   
 }
+componentDidMount(){
+  console.log("helloooo")
+  console.log(this.props.data.hourly[0].time)
+  var date = this.props.data.hourly[0].time
+  date = date.toString()
+  this.setState({
+    address:this.props.data.address,
+    date:date
+  })
+}
 
 
 render() {
   return (
       <>  
+          <h1>{this.state.address}</h1>
+          <p>{this.state.date}</p>
           <Dropdown data={this.props.data.hourly}/>
           <p>Press the button!</p>
     </>
