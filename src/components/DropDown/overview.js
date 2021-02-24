904ba088231c6bcb8510412d9d25cf0d9f3f2ccf
asatvilker <asatvilker@users.noreply.github.com>
2021-02-24 00:17:54 +0000

    Update overview.js

--- src/components/DropDown/overview.js
+++ src/components/DropDown/overview.js
@@ -14,22 +14,15 @@ render() {
   return (
       <>  
          <MDBRow className="pt-4">
-           <MDBCol size="6" className="d-flex flex-column justify-content-center">
+            <MDBCol size="6" className="d-flex flex-column justify-content-center">
               <h1 style={{fontSize:"8vh",fontWeight:"400"}}>{Math.round(this.props.data.hourly[0].temperature)} &#8451;</h1>
               <p>{`${this.props.address}, ${this.props.date.toString().split(" ")[0]}, ${this.props.date.toString().split(" ")[2]}`}</p>
-             
-              
-             
-
             </MDBCol>
             <MDBCol size="6">
 
             </MDBCol>
            
          </MDBRow> 
-          
-          
-          
           <Dropdown data={this.props.data.hourly}/> {/* passing hourly info to dropdown for hourly forecast*/}
           <p>Press the button!</p>
     </>
