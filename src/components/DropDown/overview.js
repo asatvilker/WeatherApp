e96aac51269d217a84e10887c542bf71e1895ea3
asatvilker <asatvilker@users.noreply.github.com>
2021-02-24 12:01:27 +0000

    fixed bug
    there was an issue sometimes when loading data into state and passing as props, the components seem to be loading before all data was poppulated, causing errors so i added conditions to check the data is ready, if not it loads a simple spinner.

--- src/components/DropDown/overview.js
+++ src/components/DropDown/overview.js
@@ -8,18 +8,44 @@ class Overview extends Component {
     state = {
     
     }
+    componentDidMount(){
+       
+    }
 
     render() {
         return (
             <>  
                 <MDBRow className="pt-4">
                     <MDBCol size="6" className="d-flex flex-column justify-content-center">
-                        <h1 style={{fontSize:"8vh",fontWeight:"400"}}>{Math.round(this.props.data.hourly[0].temperature)}</h1>
-                        <h1 style={{fontSize:"8vh",fontWeight:"400"}}>&#8451;</h1>
-                        <p>{`${this.props.address}, ${this.props.date.toString().split(" ")[0]}, ${this.props.date.toString().split(" ")[2]}`}</p>
+                        {
+                            this.props.data.hourly[0] == undefined? 
+                                <div className="spinner-border" role="status">
+                                    <span className="sr-only">Loading...</span>         {/*load a spinner until data is fully loaded, revents error */}
+                                </div>
+                                
+                                :   /* OR - depending on statement either code above will show or code below*/
+                                
+                                <>
+                                    <h1 style={{fontSize:"8vh",fontWeight:"400"}}>{Math.round(this.props.data.hourly[0].temperature)}</h1>
+                                    <h1 style={{fontSize:"8vh",fontWeight:"400"}}>&#8451;</h1>
+                                    <p>{`${this.props.address}, ${this.props.date.toString().split(" ")[0]}, ${this.props.date.toString().split(" ")[2]}`}</p>
+                                </>
+                        }
+                        
                     </MDBCol>
                     <MDBCol size="6" className="d-flex flex-column justify-content-center" >
-                        <MDBIcon icon={codes[this.props.data.hourly[0].weatherDesc]} size="8x" className="weatherIcon"/>
+                        {
+                            this.props.data.hourly[0] == undefined? 
+                                <div>
+                                </div>
+                                
+                                :
+
+                                <>
+                                    <MDBIcon icon={codes[this.props.data.hourly[0].weatherDesc]} size="8x" className="weatherIcon"/>
+                                </>
+                        }
+                        
                     </MDBCol>
                 </MDBRow> 
                 <Dropdown data={this.props.data.hourly}/> {/* passing hourly info to dropdown for hourly forecast*/}
