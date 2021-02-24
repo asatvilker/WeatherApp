27ade6593804f2ad643bf663024e27849667a929
maxjay <disgracedandbetrayed@gmail.com>
2021-02-24 10:51:18 +0000

    fixed formatting, adding package.json to .gitignore to avoid local conflicts

--- src/components/DropDown/hourlyFcstComponent.js
+++ src/components/DropDown/hourlyFcstComponent.js
@@ -1,37 +1,34 @@
 import React, { Component } from "react";
 import { MDBBtn, MDBCol, MDBCollapse, MDBContainer, MDBIcon, MDBRow } from "mdbreact";
 import codes from '../codes.json';
+
 class HourlyComponent extends Component {
-state = {
-  hour:"",
-  temp:"",
-  condition:"",
-}
+    state = {
+        hour:"",
+        temp:"",
+        condition:"",
+    }
 
-componentDidMount(){
-    
-    
-  this.setState({
-    hour:this.props.hour,
-    temp:this.props.temp,
-    condition:this.props.condition
-  })
-}
+    componentDidMount(){
+        this.setState({
+            hour:this.props.hour,
+            temp:this.props.temp,
+            condition:this.props.condition
+        })
+    }
 
-render() {
-  return (
-      <div id="hourlyComponent">
-        <MDBContainer className="d-flex flex-column justify-content-between">
-          <h1>{this.props.hour}</h1>
-          <MDBIcon icon={codes[this.props.condition]} size="3x" className="weatherIcon"/>
-          <p>{this.props.condition}</p>
-          
-          <h2 style={{fontWeight:"400"}}>{this.props.temp} &#8451;</h2>
-        </MDBContainer>
-    
-    </ div>
-    );
-  }
+    render() {
+        return (
+            <div id="hourlyComponent">
+                <MDBContainer className="d-flex flex-column justify-content-between">
+                    <h1>{this.props.hour}</h1>
+                    <MDBIcon icon={codes[this.props.condition]} size="3x" className="weatherIcon"/>
+                    <p>{this.props.condition}</p>
+                    <h2 style={{fontWeight:"400"}}>{this.props.temp} &#8451;</h2>
+                </MDBContainer>
+            </div>
+        );
+    }
 }
 
 export default HourlyComponent;
--- src/components/DropDown/overview.js
+++ src/components/DropDown/overview.js
@@ -3,32 +3,30 @@ import { MDBBtn, MDBCol, MDBCollapse, MDBContainer, MDBIcon, MDBRow } from "mdbr
 import './dropdown.css'
 import Dropdown from './dropdown';
 import codes from '../codes.json';
+
 class Overview extends Component {
-state = {
-  
-}
+    state = {
+    
+    }
 
-
-
-render() {
-  return (
-      <>  
-         <MDBRow className="pt-4">
-            <MDBCol size="6" className="d-flex flex-column justify-content-center">
-              <h1 style={{fontSize:"8vh",fontWeight:"400"}}>{Math.round(this.props.data.hourly[0].temperature)}</h1>
-              <h1 style={{fontSize:"8vh",fontWeight:"400"}}>&#8451;</h1>
-              <p>{`${this.props.address}, ${this.props.date.toString().split(" ")[0]}, ${this.props.date.toString().split(" ")[2]}`}</p>
-            </MDBCol>
-            <MDBCol size="6" className="d-flex flex-column justify-content-center" >
-              <MDBIcon icon={codes[this.props.data.hourly[0].weatherDesc]} size="8x" className="weatherIcon"/>
-            </MDBCol>
-           
-         </MDBRow> 
-          <Dropdown data={this.props.data.hourly}/> {/* passing hourly info to dropdown for hourly forecast*/}
-          <p>Press the button!</p>
-    </>
-    );
-  }
+    render() {
+        return (
+            <>  
+                <MDBRow className="pt-4">
+                    <MDBCol size="6" className="d-flex flex-column justify-content-center">
+                        <h1 style={{fontSize:"8vh",fontWeight:"400"}}>{Math.round(this.props.data.hourly[0].temperature)}</h1>
+                        <h1 style={{fontSize:"8vh",fontWeight:"400"}}>&#8451;</h1>
+                        <p>{`${this.props.address}, ${this.props.date.toString().split(" ")[0]}, ${this.props.date.toString().split(" ")[2]}`}</p>
+                    </MDBCol>
+                    <MDBCol size="6" className="d-flex flex-column justify-content-center" >
+                        <MDBIcon icon={codes[this.props.data.hourly[0].weatherDesc]} size="8x" className="weatherIcon"/>
+                    </MDBCol>
+                </MDBRow> 
+                <Dropdown data={this.props.data.hourly}/> {/* passing hourly info to dropdown for hourly forecast*/}
+                <p>Press the button!</p>
+            </>
+        );
+    }
 }
 
 export default Overview;
