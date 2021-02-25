92bbd8288e9752b9a0a49f509f7cda5aad1d89ce
asatvilker <asatvilker@users.noreply.github.com>
2021-02-25 16:14:48 +0000

    removed uneven padding in button, moved some in-line styles to style sheet

--- src/components/DropDown/overview.js
+++ src/components/DropDown/overview.js
@@ -15,8 +15,8 @@ class Overview extends Component {
     render() {
         return (
             <>  
-                <MDBRow className="pt-4">
-                    <MDBCol size="6" className="d-flex flex-column justify-content-center align-items-center">
+                <MDBRow className="pt-4 overview">
+                    <MDBCol size="6">
                         {
                             this.props.data.hourly[0] == undefined? 
                                 <div className="spinner-border" role="status">
@@ -26,16 +26,16 @@ class Overview extends Component {
                                 :   /* OR - depending on statement either code above will show or code below*/
                                 
                                 <>
-                                    <div style={{display: "flex"}}>
-                                        <h1 style={{fontSize:"8vh",fontWeight:"400"}}>{Math.round(this.props.data.hourly[0].temperature)}</h1>
-                                        <h1 style={{fontSize:"8vh",fontWeight:"400"}}>&#176;{this.props.data.celsius ? "C" : "F"}</h1>
+                                    <div className="overviewHeaderContainer">
+                                        <h1 className="overviewHeader" >{Math.round(this.props.data.hourly[0].temperature)}</h1>
+                                        <h1 className="overviewHeader" >&#176;{this.props.data.celsius ? "C" : "F"}</h1>
                                     </div>
                                     <p>{`${this.props.address}, ${this.props.date.toString().split(" ")[0]}, ${this.props.date.toString().split(" ")[2]}`}</p>
                                 </>
                         }
                         
                     </MDBCol>
-                    <MDBCol size="6" className="d-flex flex-column justify-content-center align-items-center" >
+                    <MDBCol size="6"  >
                         {
                             this.props.data.hourly[0] == undefined? 
                                 <div>
