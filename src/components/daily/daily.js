ffce898410050e51f3c24f765f64f74c0835bb3d
patej744 <patej744@outlook.com>
2021-02-27 17:06:03 +0000

    Update daily.js

--- src/components/daily/daily.js
+++ src/components/daily/daily.js
@@ -16,27 +16,25 @@ componentDidMount(){
 
 render() {
   return (
-      <div id="daily">
-          <p>Next 14 days</p>
-        <MDBContainer className="d-flex flex-row hourlyContainer" style={{margin: "0 0"}}>
-            {
-                this.props.data.map(data => {
-                    var date = data.time.toString()
-                    var time= date.split(" ")[4]
-                    var hour=time.split(":")[0]
-                    hour=hour.concat(":00")
-                    var temp=Math.round(data.temperature)
-                    var day=date.split(" ")[0]
-                    console.log(this.props.data)
-                    return(
-                        <Card hour={day} temp={temp} condition={data.weatherDesc} celsius={this.props.celsius}/>
-                    )
-                    
-                })
-            }
-        
-        </MDBContainer>
-    </ div>
+        <div id="daily">
+            <p>Next 14 days</p>
+            <MDBContainer className="d-flex flex-row hourlyContainer" style={{margin: "0 0"}}>
+                {
+                    this.props.data.map(data => {
+                        var date = data.time.toString()
+                        var time= date.split(" ")[4]
+                        var hour=time.split(":")[0]
+                        hour=hour.concat(":00")
+                        var temp=Math.round(data.temperature)
+                        var day=date.split(" ")[0]
+                        console.log(this.props.data)
+                        return(
+                            <Card hour={day} temp={temp} condition={data.weatherDesc} celsius={this.props.celsius}/>
+                        )
+                    })
+                }
+            </MDBContainer>
+        </ div>
     );
   }
 }
