9ef1fea96709e840ce62f9bc35870d313bf556df
maxjay <disgracedandbetrayed@gmail.com>
2021-03-06 13:21:36 +0000

    gave weatherIcons width: 100% to center

--- src/components/weatherIcons.js
+++ src/components/weatherIcons.js
@@ -3,7 +3,7 @@ import * as WeatherIcons from "react-icons/wi";
 const WeatherIcon = ({iconName, size}) => {
     let Icon = WeatherIcons[iconName];
     return (
-        <Icon className="weatherIcon" size={size} style={{color: "floralwhite"}}/>
+        <Icon className="weatherIcon" size={size} style={{color: "floralwhite", width: "100%"}}/>
     )
 }
 
