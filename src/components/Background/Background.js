ba3c215ac19db1bb8456f4ae038c491c93810079
patej744 <patej744@outlook.com>
2021-03-07 23:12:32 +0000

    adjusted background coding

--- src/components/Background/Background.js
+++ src/components/Background/Background.js
@@ -1,78 +1,108 @@
 import React from 'react';
 import './Background.css';
 
-function Mountain(props) {
+function Mountain() {
   return (
     <div className="mountains">
       <div className="leftWrap">
-        <div className='mountainLeft' style={{backgroundColor : props.color}}></div>
+        <div className='mountainLeft'></div>
       </div>
       <div className="rightWrap">
-        <div className='mountainRight' style={{backgroundColor : props.color}}></div>
+        <div className='mountainRight'></div>
       </div>
       <div className="midWrap">
-        <div className='mountainMid' style={{backgroundColor : props.color}}></div>
+        <div className='mountainMid'></div>
       </div>
     </div>
   );
 }
 
-function Sun(props) {
+function Sun() {
   return(
     <div className="sun-wrap">
-      <div className="sun" id={props.idName}></div>
+      <div className="sun"></div>
     </div>
   );
 }
 
 function TimeChange() {
   var today = new Date();
-  const ids = ['time10-14','time14-18','time18-21','time21-23','time23-4','time4-6','time6-8','time8-10'];
-  const colors = ['rgb(255, 125, 69)','rgb(248, 177, 149)','rgb(246, 114, 128)','rgb(192, 108, 132)','rgb(53, 92, 125)','rgb(192, 108, 180)','rgb(246, 114, 128)','rgb(248, 177, 149)']
-  const backgrounds = ['linear-gradient(180deg, #F8B195 0%, rgba(255, 125, 69, 0.3) 100%)','linear-gradient(180deg, #FF7D45 0%, rgba(248, 177, 149, 0.3) 100%)','linear-gradient(180deg, #F8B195 0%, rgba(246, 114, 128, 0) 100%)','linear-gradient(180deg, #F67280 0%, rgba(192, 108, 132, 0.3) 100%)','linear-gradient(180deg, #6C5B7B 0%, rgba(53, 92, 125, 0.3) 100%)','linear-gradient(180deg, #6C5B7B 0%, #6C5B7B 0.01%, rgba(192, 108, 180, 0.3) 100%)','linear-gradient(180deg, #C06CB4 0%, rgba(246, 114, 128, 0.3) 100%)','linear-gradient(180deg, #F67280 0%, rgba(248, 177, 149, 0) 100%)'];
-  var currentId;
+  const colors = ['248, 177, 149','255, 125, 69','246, 114, 128','192, 108, 132','53, 92, 125','192, 108, 180','246, 114, 128','248, 177, 149']
+  const backgrounds = ['linear-gradient(180deg, #FF7D45 20%, rgba(248, 177, 149, 1) 100%)','linear-gradient(180deg, #F8B195 20%, rgba(255, 125, 69, 1) 100%)','linear-gradient(180deg, #F8B195 20%, rgba(246, 114, 128, 1) 100%)','linear-gradient(180deg, rgba(246, 114, 128, 0.7) 20%, rgba(192, 108, 132, 0.8) 100%)','linear-gradient(180deg, #6C5B7B 20%, rgba(53, 92, 125, 1) 100%)','linear-gradient(180deg, #6C5B7B 20%, #6C5B7B, rgba(192, 108, 180, 1) 100%)','linear-gradient(180deg, #C06CB4 20%, rgba(246, 114, 128, 1) 100%)','linear-gradient(180deg, #F67280 20%, rgba(248, 177, 149, 1) 100%)'];
   var currentColor;
   var bodyColor;
+  let curTime = today.getHours();
+  if(curTime>=0 && curTime<4) {
+    curTime = 23;
+  }
 
-  if(today.getHours()<14 && today.getHours()>=10) {
-    currentId=ids[0];
+  if(curTime<15 && curTime>=10) {
     currentColor=colors[0];
     bodyColor = backgrounds[0];
-  } else if(today.getHours()<18 && today.getHours()>=14) {
-    currentId=ids[1];
+  } else if(curTime<18 && curTime>=15) {
     currentColor=colors[1];
     bodyColor = backgrounds[1];
-  } else if(today.getHours()<21 && today.getHours()>=18) {
-    currentId=ids[2];
+  } else if(curTime<20 && curTime>=18) {
     currentColor=colors[2];
     bodyColor = backgrounds[2];
-  } else if(today.getHours()<23 && today.getHours()>=21) {
-    currentId=ids[3];
+  } else if(curTime<22 && curTime>=20) {
     currentColor=colors[3];
     bodyColor = backgrounds[3];
-  } else if(today.getHours()<4 && today.getHours()>=23) {
-    currentId=ids[4];
+  } else if(curTime>=22) {
     currentColor=colors[4];
     bodyColor = backgrounds[4];
-  } else if(today.getHours()<6 && today.getHours()>=4) {
-    currentId=ids[5];
+  } else if(curTime<6 && curTime>=4) {
     currentColor=colors[5];
     bodyColor = backgrounds[5];
-  } else if(today.getHours()<8 && today.getHours()>=6) {
-    currentId=ids[6];
+  } else if(curTime<8 && curTime>=6) {
     currentColor=colors[6];
     bodyColor = backgrounds[6];
-  } else {
-    currentId=ids[7];
+  } else if(curTime<10 && curTime>=8) {
     currentColor=colors[7];
     bodyColor = backgrounds[7];
   }
   
   return (
     <>
-      <style>{`body {background: ${bodyColor};}`}</style>
-      <Sun idName={currentId} />
-      <Mountain color={currentColor}/>
+      <style>{
+      `
+        body {
+          background: ${bodyColor};
+        }
+
+        .mountainMid,
+        .mountainRight,
+        .mountainLeft {
+          background-color: rgb(${currentColor});
+        }
+
+
+        .sun {
+          background-color: rgb(${currentColor});
+          box-shadow: 0 0 0 60px rgba(${currentColor}, 0.4),
+          0 0 0 120px rgba(${currentColor}, 0.3), 0 0 0 180px rgba(${currentColor}, 0.2),
+          0 0 0 240px rgba(${currentColor}, 0.1), 0 0 0 300px rgba(${currentColor}, 0),
+          0 0 200px 300px rgba(${currentColor}, 0);
+        }
+      
+        @keyframes rays {
+          0% {
+            box-shadow: 0 0 0 0px rgba(${currentColor}, 0.4),
+              0 0 0 60px rgba(${currentColor}, 0.4), 0 0 0 120px rgba(${currentColor}, 0.3),
+              0 0 0 180px rgba(${currentColor}, 0.2), 0 0 0 240px rgba(${currentColor}, 0.1),
+              0 0 200px 240px rgba(${currentColor}, 0.1);
+          }
+          100% {
+            background-color: rgb(${currentColor});
+          box-shadow: 0 0 0 60px rgba(${currentColor}, 0.4),
+          0 0 0 120px rgba(${currentColor}, 0.3), 0 0 0 180px rgba(${currentColor}, 0.2),
+          0 0 0 240px rgba(${currentColor}, 0.1), 0 0 0 300px rgba(${currentColor}, 0),
+          0 0 200px 300px rgba(${currentColor}, 0);
+          }
+        }
+      `}</style>
+      <Sun/>
+      <Mountain/>
     </>
   );
 }
