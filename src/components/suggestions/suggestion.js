cc80f273eb9e473d908a0e815d577f0f8db72df9
tarkh-cell <tarikhmukhtar@gmail.com>
2021-03-10 22:45:04 +0000

    second draft - added rain and swap button for graph

--- src/components/suggestions/suggestion.js
+++ src/components/suggestions/suggestion.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import './suggestion.css';
+import { MDBIcon } from 'mdbreact';
 
 class Suggest extends Component{
   constructor(props){
     super(props);
     this.state = {
       selection:[],
-      hourly:[]
+      hourly:[],
+      addRain:""
     }
   }
 
@@ -22,15 +24,22 @@ class Suggest extends Component{
       } else if (Math.round(this.props.hourly[0].temperature) >= 15) {
         this.setState({selection:["wear a jacket"]});
       } else if (Math.round(this.props.hourly[0].temperature) >= 10) {
-        this.setState({selection:["wear a wooly hat"]});
+        this.setState({selection:["wear a jumper"]});
       } else if (Math.round(this.props.hourly[0].temperature) >= 5) {
         this.setState({selection:["wear a scarf"]});
       } else if (Math.round(this.props.hourly[0].temperature) >= 0) {
-        this.setState({selection:["wear a coat"]});
+        this.setState({selection:["wear a wooly hat"]});
       } else if (Math.round(this.props.hourly[0].temperature) >= -5) {
-        this.setState({selection:["wear a egg"]});
+        this.setState({selection:["wear a coat"]});
       } else if (Math.round(this.props.hourly[0].temperature) < -5) {
         this.setState({selection:["wear a snowsuit"]});
+      } else {
+        this.setState({selection:["wear a hoody"]});
+      }
+      if (this.props.hourly[0].weatherDesc.includes("rain")){
+        this.setState({addRain:"and bring an umbrella"});
+      } else {
+        this.setState({addRain:""});
       }
     }
     }
@@ -38,8 +47,11 @@ class Suggest extends Component{
   render(){
     return(
       <div id='main'>
+        <div id='swap'> 
+          <button id='flip'><MDBIcon icon="exchange-alt" size="lg" /></button>
+        </div>
         <div id='text'>
-          Suggestion: {this.state.selection}
+          Suggestion: {this.state.selection} {this.state.addRain}
         </div>
         <hr />
       </div>
