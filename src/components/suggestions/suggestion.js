4e0e59f16e4b93291b8cbcb47d13fa299ff1a191
asatvilker <asatvilker@users.noreply.github.com>
2021-03-26 02:45:27 +0000

    improved scroll when on phone

--- src/components/suggestions/suggestion.js
+++ src/components/suggestions/suggestion.js
@@ -6,7 +6,7 @@ class Suggest extends Component{
   constructor(props){
     super(props);
     this.state = {
-      selection:[],
+      selection:"",
       hourly:[],
       addRain:"",
       rainOption:["Drizzle","Rain","Light Rain","Heavy Rain","Flurries","Light Snow","Heavy Snow","Freezing Drizzle","Freezing Rain","Light Freezing Rain","Heavy Freezing Rain","Showers", "Mostly cloudy w/ showers", "Partly sunny w/ showers", "Rain and snow", "Partly cloudy w/ showers"],
@@ -20,25 +20,25 @@ class Suggest extends Component{
       const temp=this.props.celsius? this.props.hourly[0].temperature: (this.props.hourly[0].temperature - 32)* 5/9 //ensures temperate is in celsius (consistent format which logic below is based on)
       const isRaining= this.state.rainOption.includes(this.props.hourly[0].weatherDesc)
       if (temp >= 30) {
-        this.setState({selection:["Bring some sunglasses"]});
+        this.setState({selection:"Bring some sunglasses"});
       } else if (temp >= 25) {
-        this.setState({selection:["Might want a sun hat"]});
+        this.setState({selection:"Might want a sun hat"});
       } else if (temp >= 20) {
-        this.setState({selection:["wear a T-shirt"]});
+        this.setState({selection:"wear a T-shirt"});
       } else if (temp >= 15) {
-        this.setState({selection:["Grab a jacket"]});
+        this.setState({selection:"Grab a jacket"});
       } else if (temp >= 10) {
-        this.setState({selection:["Wear a jumper"]});
+        this.setState({selection:"Wear a jumper"});
       } else if (temp >= 5) {
-        this.setState({selection:["Don't forget a scarf"]});
+        this.setState({selection:"Don't forget a scarf"});
       } else if (temp >= 0) {
-        this.setState({selection:["Might want a wooly hat"]});
+        this.setState({selection:"Might want a wooly hat"});
       } else if (temp >= -5) {
-        this.setState({selection:["Grab a coat"]});
+        this.setState({selection:"Grab a coat"});
       } else if (temp < -5) {
-        this.setState({selection:["Wear a snowsuit"]});
+        this.setState({selection:"Wear a snowsuit"});
       } else {
-        this.setState({selection:["Wear a hoodie"]});
+        this.setState({selection:"Wear a hoodie"});
       }
       if (isRaining){
         this.setState({addRain:"and bring an umbrella"});
@@ -55,7 +55,7 @@ class Suggest extends Component{
           <button id='flip'><MDBIcon icon="exchange-alt" size="lg" /></button>
         </div>
         <div id='text'>
-          {this.state.selection} {this.state.addRain}
+          <strong>{this.state.selection} {this.state.addRain}</strong>
         </div>
         <hr />
       </div>
