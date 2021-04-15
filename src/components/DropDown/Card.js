f7b7fc158dd617d4c8e3a87a97b165a87e8be001
asatvilker <asatvilker@users.noreply.github.com>
2021-04-15 22:28:23 +0100

    units were moving weird and not updating

--- src/components/DropDown/Card.js
+++ src/components/DropDown/Card.js
@@ -19,7 +19,7 @@ class Card extends Component {
                 <WeatherIcon iconName={this.props.icon} size="9vh"></WeatherIcon> {/*displays icon component passing through name of icon */}
                 <h1>{this.props.time}</h1> {/*if hourly then this displays hour, if daily it will be the day */}
                 <p>{this.props.condition}</p>
-                <p>{this.props.wind.speed.value.toFixed(1)}{this.props.wind.speed.unit}</p>
+                <p>{this.props.wind.speed.value.toFixed(1)}{this.props.kmh?"km/h":"mph"}</p>
                 <h2 style={{fontWeight:"400"}}>{this.props.temp}&#176;{this.props.celsius ? "C" : "F"}</h2> {/*checks if in celcius or farenheight, displays appropriate symbol */}
             </MDBContainer>
 
